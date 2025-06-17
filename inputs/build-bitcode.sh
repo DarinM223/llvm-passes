6a8ec592a7731999c0ffafc8293e93c11672d557
d <darinm223@gmail.com>
2025-06-17 08:39:20 -0700

    Add script to build input with bitcode

---
+++ inputs/build-bitcode.sh
@@ -0,0 +1,1 @@
+clang-19 -emit-llvm -c $1.c -o $1.bc
--- run-opcode-counter.sh
+++ run-opcode-counter.sh
@@ -1,1 +1,1 @@
-opt-19 -load-pass-plugin ./libOpcodeCounter.so -passes=opcode-counter -disable-output inputs/input_for_cc.ll
+opt-19 -load-pass-plugin ./libOpcodeCounter.so -passes=opcode-counter -disable-output inputs/input_for_cc.bc
